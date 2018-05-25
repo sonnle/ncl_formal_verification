cb6a3011fa34f9b2dd76d42b03d75453175830ae
sonnle <son.n.le@hotmail.com>
2018-05-24 19:34:42 -0500

    Allow for enable/disable of certain tests

--- scripts/input_completeness/multiplier/run_multiplier.py
+++ scripts/input_completeness/multiplier/run_multiplier.py
@@ -14,7 +14,7 @@ from UMultProofNullRelaxBuggy import UMultProofNullRelaxBuggy
 from UMultProofDataRelaxBuggy import UMultProofDataRelaxBuggy
 
 def main():
-    start_bit = 11
+    start_bit = 3
     num_bits_end = 15
     num_bits = num_bits_end + 1
 
@@ -26,6 +26,17 @@ def main():
     ]
 
     for bits in range(start_bit, num_bits):
+        test_enable = [
+            1, # Null Buggy
+            1, # Data Buggy
+            1, # Null Relax Buggy
+            1, # Data Relax Buggy
+            1, # Null
+            1, # Data
+            1, # Null Relax
+            1, # Data Relax
+        ]
+
         test_obj = [
             UMultProofNullBuggy(bits),
             UMultProofDataBuggy(bits),
@@ -50,26 +61,27 @@ def main():
 
 
         for index, test in enumerate(test_obj):
-            current_file_name = test_smt_name[index].format(bits)
-            with open(current_file_name, 'w') as w_file:
-                w_file.write(test.generate_smt_proof())
+            if test_enable[index]:
+                current_file_name = test_smt_name[index].format(bits)
+                with open(current_file_name, 'w') as w_file:
+                    w_file.write(test.generate_smt_proof())
 
-            try:
-                results_file = open('results.txt', 'a')
-                results_file.write('Results for {0}:\n'.format(current_file_name))
-                start = timeit.default_timer()
-                results_file.write(subprocess.check_output('z3.exe -smt2 {0}'.format(current_file_name), shell=True))
-                print 'Model returned SAT for {0}'.format(current_file_name)
-            except subprocess.CalledProcessError as e:
-                results_file.write(e.output)
-                if 'unsat' in e.output:
-                    print 'Model returned UNSAT for {0}'.format(current_file_name)
-                else:
-                    print 'Model has syntax errors, check results for error'
-            time_taken = timeit.default_timer() - start
-            results_file.write('Time taken: %.3f\n\n' % (time_taken))
-            test_times[index].append('%.3f' % (time_taken))
-            results_file.close()
+                try:
+                    results_file = open('results.txt', 'a')
+                    results_file.write('Results for {0}:\n'.format(current_file_name))
+                    start = timeit.default_timer()
+                    results_file.write(subprocess.check_output('z3.exe -smt2 {0}'.format(current_file_name), shell=True))
+                    print 'Model returned SAT for {0}'.format(current_file_name)
+                except subprocess.CalledProcessError as e:
+                    results_file.write(e.output)
+                    if 'unsat' in e.output:
+                        print 'Model returned UNSAT for {0}'.format(current_file_name)
+                    else:
+                        print 'Model has syntax errors, check results for error'
+                time_taken = timeit.default_timer() - start
+                results_file.write('Time taken: %.3f\n\n' % (time_taken))
+                test_times[index].append('%.3f' % (time_taken))
+                results_file.close()
 
         for index, time_list in enumerate(test_times):
             time_file_name = 'runtime_' + test_smt_name[index].format('N').rstrip('.smt2') + '.txt'
